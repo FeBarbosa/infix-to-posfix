6b7e0196696ea21bf3e20c1f8c5702a57e087946
febarbosa <felipe.barbosa@mail.uft.edu.br>
2021-05-24 01:23:32 -0300

    regex class structure

---
+++ regex.py
@@ -0,0 +1,18 @@
+
+class regex:
+    
+    def __init__(self, inFix):
+        self.inFix = inFix
+        self.posFix = ""
+
+    def isValid(self):
+        pass
+
+    def __infixToPosfix(self):
+        pass
+
+    def __precedence(self, character1, character2):
+        pass
+
+    def __isOperator(self, character):
+        pass
